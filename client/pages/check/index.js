dc5c9108193e875b894dfc3f0a53db306b36a095
逸莱 <yilai.zp@alibaba-inc.com>
2018-06-27 16:11:56 +0800

    fix bug

--- client/pages/check/index.js
+++ client/pages/check/index.js
@@ -40,6 +40,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    wx.setNavigationBarTitle({ title: '通关测验' });
     wx.getSystemInfo({
       success: (res) => {
         this.setData({
